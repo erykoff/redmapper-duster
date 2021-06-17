c7fdccc792678a7626b2b0957278cc8bbc860f19
Eli Rykoff <erykoff@stanford.edu>
2021-06-17 14:19:13 -0700

    Fix flake8 errors.

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,7 +4,7 @@ import unittest
 class ImportTestCase(unittest.TestCase):
     def test_import(self):
         """Test trivial import."""
-        import duster
+        import duster  # noqa: F401
 
 
 if __name__ == '__main__':
