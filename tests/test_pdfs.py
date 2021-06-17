39749d7f8a1b946cf62ecaebaac2e72e26f19301
Eli Rykoff <erykoff@stanford.edu>
2021-06-17 16:23:02 -0700

    Add p_dust and normalization and test.

---
+++ tests/test_pdfs.py
@@ -0,0 +1,36 @@
+import unittest
+import numpy as np
+import scipy.integrate
+import numpy.testing as testing
+
+from duster import pdfs
+
+
+class PdfTestCase(unittest.TestCase):
+    def test_p_dust(self):
+        """Test p_dust."""
+        np.random.seed(12345)
+
+        ntest = 1000
+
+        # Compute a random selection of positive u, b values
+        b = np.random.uniform(low=0.1, high=20.0, size=ntest)
+        u = np.random.uniform(low=0.01, high=20.0, size=ntest)
+
+        rho_vals = np.arange(0.0, 10.0, 0.001) + 0.0001
+
+        integrals = np.zeros(ntest)
+
+        for i in range(ntest):
+            # Compute the normalization and p_dust
+            rho_0, rho_min = pdfs.compute_normalized_rho_pars(u[i], b[i])
+            p_dust = pdfs.p_dust(rho_0, b[i], rho_min, rho_vals)
+
+            integrals[i] = scipy.integrate.simps(p_dust, rho_vals)
+
+        # Ensure that the normalization is 1.0
+        testing.assert_array_almost_equal(integrals, 1.0, decimal=4)
+
+
+if __name__ == '__main__':
+    unittest.main()
